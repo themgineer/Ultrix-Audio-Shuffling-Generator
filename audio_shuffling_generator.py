85305421484f82341fd3650b0c5a5a52ec1f093e
Anthony Thompson <anthonythompson@me.com>
2024-02-29 10:24:52 -0500

    Updated code per PEP8

--- audio_shuffling_generator.py
+++ audio_shuffling_generator.py
@@ -1,3 +1,8 @@
+"""
+This creates a GUI that will easily generate names and port labels
+for Audio Shuffling Sources for an Ross Video Ultrix.
+"""
+
 import os
 import sys
 import csv
@@ -5,15 +10,15 @@ import PySimpleGUI as gui
 
 
 class OutputEmpty(Exception):
-    pass
+    """Creates custom OutputEmpty Exception"""
 
 
 class InputEmpty(Exception):
-    pass
+    """Creates custom InputEmpty Exception"""
 
 
 class InvalidGroup(Exception):
-    pass
+    """Creates custom InvalidGroup Exception"""
 
 
 def img_resource_path(relative_path):
@@ -30,6 +35,7 @@ def img_resource_path(relative_path):
 # Function that runs if input file is detected as a list instead of a csv.
 # It recurses through the sources list and appends the channel number to it.
 def run_as_list(channels, group_step, sources, output, out_file):
+    """"Processes input file as a list of names."""
 
     if group_step == 1:
         for i in sources:
@@ -44,7 +50,9 @@ def run_as_list(channels, group_step, sources, output, out_file):
 
     return write_list(out_file, output)
 
-def run_as_list_LZ(channels, group_step, sources, output, out_file):
+
+def run_as_list_leading_zero(channels, group_step, sources, output, out_file):
+    """"Processes input file as a list with leading zeroes"""
 
     if group_step == 1:
         for i in sources:
@@ -62,6 +70,8 @@ def run_as_list_LZ(channels, group_step, sources, output, out_file):
 
 # Function that runs if input file is detected as a csv instead of a list.
 def run_as_dict(channels, group_step, sources, output, out_file):
+    """Processes input file as a CSV"""
+
     fields = ["Name", "Description", "Video"]
     for f in range(1, channels+1):
         fields.append(f"Audio {f}")
@@ -69,12 +79,12 @@ def run_as_dict(channels, group_step, sources, output, out_file):
     if group_step == 1:
         for i in sources:
             for j in range(1, channels + 1):
-                temp = ([f"{i} CH{j}"])
+                temp = [f"{i} CH{j}"]
                 temp.extend(["", ""])
                 for k in range(1, channels + 1):
                     temp.append(f"{sources[i]}.audio.ch{j}")
-                tempDict = dict(zip(fields, temp))
-                output.append(tempDict)
+                temp_dict = dict(zip(fields, temp))
+                output.append(temp_dict)
     else:
         for i in sources:
             for j in range(1, channels + 1, group_step):
@@ -83,14 +93,17 @@ def run_as_dict(channels, group_step, sources, output, out_file):
                 for k in range(1, channels + 1, group_step):
                     for m in range(j, j + group_step):
                         temp.append(f"{sources[i]}.audio.ch{m}")
-                tempDict = dict(zip(fields, temp))
-                output.append(tempDict)
+                temp_dict = dict(zip(fields, temp))
+                output.append(temp_dict)
 
     out_file = get_filename(out_file)
 
     return write_dict(out_file, output, fields)
 
-def run_as_dict_LZ(channels, group_step, sources, output, out_file):
+
+def run_as_dict_leading_zero(channels, group_step, sources, output, out_file):
+    """Processes input file as CSV with leading zeroes"""
+
     fields = ["Name", "Description", "Video"]
     for f in range(1, channels+1):
         fields.append(f"Audio {f}")
@@ -98,12 +111,12 @@ def run_as_dict_LZ(channels, group_step, sources, output, out_file):
     if group_step == 1:
         for i in sources:
             for j in range(1, channels + 1):
-                temp = ([f"{i} CH{j:02d}"])
+                temp = [f"{i} CH{j:02d}"]
                 temp.extend(["", ""])
                 for k in range(1, channels + 1):
                     temp.append(f"{sources[i]}.audio.ch{j}")
-                tempDict = dict(zip(fields, temp))
-                output.append(tempDict)
+                temp_dict = dict(zip(fields, temp))
+                output.append(temp_dict)
     else:
         for i in sources:
             for j in range(1, channels + 1, group_step):
@@ -112,41 +125,45 @@ def run_as_dict_LZ(channels, group_step, sources, output, out_file):
                 for k in range(1, channels + 1, group_step):
                     for m in range(j, j + group_step):
                         temp.append(f"{sources[i]}.audio.ch{m}")
-                tempDict = dict(zip(fields, temp))
-                output.append(tempDict)
+                temp_dict = dict(zip(fields, temp))
+                output.append(temp_dict)
 
     out_file = get_filename(out_file)
 
     return write_dict(out_file, output, fields)
 
 
-# Function designed to output the list to a file.
-def write_list(filename, list):
-    with open(filename, 'w', newline='') as out:
+def write_list(filename, output_list):
+    """Function designed to output the list to a file."""
+    with open(filename, 'w', encoding='UTF-8', newline='') as out:
         writer = csv.writer(out)
-        writer.writerows(list)
+        writer.writerows(output_list)
     return "Output successful!"
 
 
-# Function designed to generate csv that matches Ultrix database layout.
 def write_dict(out_file, out_dict, fields):
-    with open(out_file, 'w', newline='') as out:
+    """Function designed to generate csv that matches Ultrix database layout."""
+    with open(out_file, 'w', encoding='UTF-8', newline='') as out:
         writer = csv.DictWriter(out, fieldnames=fields)
         writer.writeheader()
         writer.writerows(out_dict)
     return "Output successful!"
 
 
-# Function that checks if an output path or filename was provided.
-# If not, uses default 'shuffled_audio.csv'.
 def get_filename(out_file):
+    """
+    Function that checks if an output path or filename was provided.
+    If not, uses default 'shuffled_audio.csv'.
+    """
+
     if out_file.split(".")[-1] != "csv":
         out_file += ".csv"
 
     return out_file
 
 
-def process_file(list, channels, grouping, out_file, LZ):
+def process_file(source_list, channels, grouping, out_file, leading_zero):
+    """Function that processes in the input file"""
     # Initialize output list
     output = []
     group_dict = {"Mono": 1, "Stereo": 2, "Quad": 4, "Octo": 8}
@@ -165,18 +182,18 @@ def process_file(list, channels, grouping, out_file, LZ):
     # it will attempt to use 'sources.txt', but will fail if it cannot find it.
 
     try:
-        if list == "" or list is None:
+        if source_list == "" or source_list is None:
             raise InputEmpty()
         elif out_file == "" or out_file is None:
             raise OutputEmpty()
 
         sources = {}
-        with open(list, mode='r') as src:
+        with open(source_list, mode='r', encoding='UTF-8') as src:
             read = csv.reader(src)
             sources = {rows[0]: rows[1] for rows in read}
-        
-        if LZ:
-            dict_return = run_as_dict_LZ(
+
+        if leading_zero:
+            dict_return = run_as_dict_leading_zero(
                 channels, group_step, sources, output, out_file)
         else:
             dict_return = run_as_dict(
@@ -193,18 +210,18 @@ def process_file(list, channels, grouping, out_file, LZ):
         try:
             sources = []
 
-            with open(list, mode='r') as src:
+            with open(source_list, mode='r', encoding='UTF-8') as src:
                 for line in src:
                     if line[0] == '\n' or line[0] == '#':
                         continue
                     sources.append(line.strip())
 
-            if LZ:
-                return_list = run_as_list_LZ(channels,
-                                             group_step,
-                                             sources,
-                                             output,
-                                             out_file)
+            if leading_zero:
+                return_list = run_as_list_leading_zero(channels,
+                                                       group_step,
+                                                       sources,
+                                                       output,
+                                                       out_file)
             else:
                 return_list = run_as_list(channels,
                                           group_step,
@@ -222,6 +239,7 @@ def process_file(list, channels, grouping, out_file, LZ):
 
 
 def get_folder(path):
+    """Function that pulls the folder path from input file"""
     if path == "":
         out_path = ""
     else:
@@ -233,8 +251,9 @@ def get_folder(path):
 
 
 def main():
+    """Main function"""
 
-    icon_path=img_resource_path("icon/Ultrix_U.ico")
+    icon_path = img_resource_path("icon/Ultrix_U.ico")
 
     # GUI Theme
     gui.theme('reddit')
@@ -261,7 +280,7 @@ def main():
                          default_value='Mono',
                          readonly=True),
                gui.Checkbox(text='Leading Zeroes',
-                            key='LZ',
+                            key='leading_zero',
                             pad=((15, 0), (0, 0)),
                             background_color='#D9D6D1',
                             checkbox_color='#FFFFFF')],
@@ -294,7 +313,7 @@ def main():
                                    values['channels'],
                                    values['grouping'],
                                    values['out_file'],
-                                   values['LZ'])
+                                   values['leading_zero'])
             window['out_message'].update(message)
         elif event == "source_file":
             window['out_file'].update(get_folder(values['source_file']))
